8deb8dedd6159e32414a7cff9f741683c280e731
datpmiscape <datpm.iscape@gmail.com>
2025-01-03 14:22:11 +0700

    State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,33 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "DatPMiScape",
+    channel: "HAB",
+  };
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
-    return <div>hello my component HAB</div>;
+    let nameP = "DatPMiScape";
+
+    return (
+      <>
+        <div className="first">hello my component {nameP}</div>
+        <div className="second">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          hello my component {this.state["name"]}
+        </div>
+        <div className="third">hello my component {this.state.channel}</div>
+      </>
+    );
   }
 }
 
